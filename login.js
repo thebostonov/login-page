4cd4b0961922540d2af2b72981b233584725a805
thebostonov <bbostonov03@gmail.com>
2022-03-05 16:42:35 +0500

    first commit

---
+++ login.js
@@ -0,0 +1,32 @@
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector(".darkmode-toggle");
+// check if dark mode is enabled
+// if it's enabled, turn it off
+// if it's disabled, turn it on
+
+const enableDarkMode = () => {
+  // 1.add the class darkmode to the body
+  document.body.classList.toggle("darkmode");
+  // 2.update darkMode in the LocalStorage
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  // 1.add the class darkmode to the body
+  document.body.classList.toggle("darkmode");
+  // 2.update darkMode in the LocalStorage
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
