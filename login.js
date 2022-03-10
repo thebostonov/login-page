ffb5902bf4077027a49002c0935977be83c0af3c
thebostonov <bbostonov03@gmail.com>
2022-03-10 18:49:09 +0500

    lang select

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const langToggle = document.querySelector(".lang-toggle");
 const langSelect = document.querySelector(".lang-select");
+
 let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector(".darkmode-toggle");
 // check if dark mode is enabled
@@ -35,4 +36,36 @@ darkModeToggle.addEventListener("click", () => {
 
 langSelect.addEventListener("click", function () {
   langToggle.classList.toggle("lang-open");
+});
+
+const inputPlaceholder = document.querySelector(".input-bottom");
+const inputPlace = document.querySelector(".login-input");
+const buttonDesc = document.querySelector(".login-button");
+const langMainDesc = document.querySelector(".lang-select-desc");
+const langBtns = document.querySelectorAll(".lang-btn");
+const langLinks = document.querySelectorAll(".lang-btn-link");
+const langDesc = document.querySelectorAll(".lang-btn-desc");
+langLinks.forEach(function (langLink) {
+  langLink.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    if (id == "uz") {
+      langMainDesc.innerHTML = "Uzbek";
+      userphone.setAttribute("placeholder", "Telfon");
+      userpass.setAttribute("placeholder", "Parol");
+      remember.textContent = "Meni eslabqol";
+      enterbtn.textContent = "Kirish";
+    } else if (id == "ru") {
+      langMainDesc.innerHTML = "Russian";
+      userphone.setAttribute("placeholder", "Телефон");
+      userpass.setAttribute("placeholder", "Пароль");
+      remember.textContent = "Запомни меня";
+      enterbtn.textContent = "Войти";
+    } else if (id == "en") {
+      langMainDesc.innerHTML = "English";
+      userphone.setAttribute("placeholder", "Phone");
+      userpass.setAttribute("placeholder", "Password");
+      remember.textContent = "Remember me";
+      enterbtn.textContent = "Enter";
+    }
+  });
 });
